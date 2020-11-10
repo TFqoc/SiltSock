8ac901c2471e7d8dcb5fd7d6bd09b1dc4450d3d5
Tristan-Fullmer <ful15007@byui.edu>
2020-11-10 10:40:38 -0600

    update views.xml

--- debtor_status/models/debtor.py
+++ debtor_status/models/debtor.py
@@ -6,5 +6,6 @@ from odoo import models, fields
 class DebtorContact(models.Model):
     _inherit = 'res.partner'
     _name = 'debtor.status'
+    _description = "Adds the trust field to the contact page"
 
     debtor_status = fields.Many2one(comodel_name='trust')
