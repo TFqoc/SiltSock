d11af36e75f8e94183e71b6f2a15a510fd22f14d
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-09 09:40:06 -0500

    Update models.py

--- debtor_status/models/models.py
+++ debtor_status/models/models.py
@@ -3,10 +3,9 @@ from odoo import models, fields, api
 class Product(models.Model):
     _inherit = 'product.product'
     
-    @api.multi
     def name_get(self):
         res = []
         for record in self:
-            name = str(record.name)
+            name = 'Silly name'#str(record.name)
             res.append((record.id, name))
         return res
