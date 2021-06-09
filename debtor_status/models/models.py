be7c2303d4b32af62ac4872ed464214372e6a807
Tristan-Fullmer <tfullmer@qocinnovations.com>
2021-06-09 09:48:03 -0500

    actually load the models directory

--- debtor_status/models/models.py
+++ debtor_status/models/models.py
@@ -6,6 +6,6 @@ class Product(models.Model):
     def name_get(self):
         res = []
         for record in self:
-            name = 'Silly name'#str(record.name)
+            name = str(record.name)
             res.append((record.id, name))
         return res
