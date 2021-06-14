519b6d8ae5cc9c57ef67233ca68e90277114c94b
JMC-ai <80903570+JMC-ai@users.noreply.github.com>
2021-06-14 10:52:40 -0500

    Updated Manifest and Removed unnecessary file.

--- sale_report_modifications/__manifest__.py
+++ sale_report_modifications/__manifest__.py
@@ -22,10 +22,10 @@
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
         'views/views.xml',
-        'report/invoice_report.xml'
-        #'views/templates.xml',
+        'report/invoice_report.xml',
+        'views/views.xml',
+        'views/templates.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
